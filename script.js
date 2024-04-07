960f02421863d6cf8ea3658c92a3eced6574e803
Rajatpradhan123 <pradhanrajat1999@gmai.com>
2024-04-07 23:30:07 +0530

    Add New Files

---
+++ script.js
@@ -0,0 +1,40 @@
+var t1 = gsap.timeline();
+
+t1.from ("nav",{
+
+    y:-30,
+    opacity:0,
+    delay:0.3,
+    duration:0.5,
+
+})
+
+
+t1.from (".Page1 h1",{
+
+    y:-30,
+    opacity:0,
+    delay:0.3,
+    duration:0.5,
+
+})
+
+
+t1.from (".Page1 p",{
+
+    y:-30,
+    opacity:0,
+    delay:0.3,
+    duration:0.5,
+
+})
+
+
+t1.from ("#B1> a",{
+
+    y:-30,
+    opacity:0,
+    delay:0.3,
+    duration:0.5,
+
+})
